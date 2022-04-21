3a470eed1a4495da839da7a7fc7a8174009b927e
vaishnavimeduri <meduri.vaishu676@gmail.com>
2022-04-21 16:32:16 +0530

    changes made

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ function calcInterest(){
     arr.push(document.getElementById("balance3").value)
     arr.push(document.getElementById("balance4").value)
     arr.push(document.getElementById("balance5").value)
-    
+   
     for(let i=0;i<arr.length;i++){
         let z=Number(arr[i])
+        // if(arr[i]===''){
+        //     arr1[i]=0
+        //     return
+        // }
         if(z>200000){
             arr1.push((z+((z*10)/100))-(((z+((z*10)/100))*0.2)/100))
     
@@ -24,14 +28,14 @@ function calcInterest(){
                     }
                
     }
-    if(arr1[0]!=0)
+    
     document.getElementById("balance1after").innerText=arr1[0];
-    if(arr1[1]!=0)
+   
     document.getElementById("balance2after").innerText=arr1[1];
-    if(arr1[2]!=0)
+   
     document.getElementById("balance3after").innerText=arr1[2];
-    if(arr1[3]!=0)
+    
     document.getElementById("balance4after").innerText=arr1[3];
-    if(arr1[4]!=0)
+    
     document.getElementById("balance5after").innerText=arr1[4];
     }

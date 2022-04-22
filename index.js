2c638b529131164ec5bb3a7486d25a0c832c8d19
vaishnavimeduri <meduri.vaishu676@gmail.com>
2022-04-22 23:52:28 +0530

    changes made

--- index.js
+++ index.js
@@ -1,34 +1,54 @@
-function calcInterest(){
-    let arr=[]
-    let arr1=[]
+// function calcInterest(){
+//     let arr=[]
+//     let arr1=[]
 
-    arr.push(document.getElementById("balance1").value)
-    arr.push(document.getElementById("balance2").value)
-    arr.push(document.getElementById("balance3").value)
-    arr.push(document.getElementById("balance4").value)
-    arr.push(document.getElementById("balance5").value)
+//     arr.push(document.getElementById("balance1").value)
+//     arr.push(document.getElementById("balance2").value)
+//     arr.push(document.getElementById("balance3").value)
+//     arr.push(document.getElementById("balance4").value)
+//     arr.push(document.getElementById("balance5").value)
    
-    for(let i=0;i<arr.length;i++){
-        let z=Number(arr[i])
-        if(z>200000){
-            arr1.push((z+((z*10)/100))-(((z+((z*10)/100))*0.2)/100))
+//     for(let i=0;i<arr.length;i++){
+//         let z=Number(arr[i])
+//         if(z>200000){
+//             arr1.push((z+((z*10)/100))-(((z+((z*10)/100))*0.2)/100))
     
-        }
-       else if(z>100000){
-        arr1.push((z+((z*5)/100))-(((z+((z*10)/100))*0.05)/100))
+//         }
+//        else if(z>100000){
+//         arr1.push((z+((z*5)/100))-(((z+((z*10)/100))*0.05)/100))
          
-                }
-                else {
-                arr1.push(z+(z*3)/100)
+//                 }
+//                 else {
+//                 arr1.push(z+(z*3)/100)
                   
-                    }
+//                     }
                
-    }
+//     }
 
    
-    document.getElementById("balance1after").innerText=arr1[0]
-    document.getElementById("balance2after").innerText=arr1[1]
-    document.getElementById("balance3after").innerText=arr1[2]
-    document.getElementById("balance4after").innerText=arr1[3]
-    document.getElementById("balance5after").innerText=arr1[4]
+    // document.getElementById("balance1after").innerText=arr1[0]
+    // document.getElementById("balance2after").innerText=arr1[1]
+    // document.getElementById("balance3after").innerText=arr1[2]
+    // document.getElementById("balance4after").innerText=arr1[3]
+    // document.getElementById("balance5after").innerText=arr1[4]
+// }
+function calcInterest(){
+    let arr1=[]
+    let bankbalance=document.getElementsByClassName("inputbalancenodes")
+    let updatedbankbalance=document.getElementsByClassName("output")
+    for(let i=0;i<bankbalance.length;i++){
+        
+                let z=Number(bankbalance[i].value)
+                if(z>200000){
+                    arr1.push((z+((z*10)/100))-(((z+((z*10)/100))*0.2)/100))
+                 }
+               else if(z>100000){
+                arr1.push((z+((z*5)/100))-(((z+((z*10)/100))*0.05)/100))
+                 }
+               else {
+                arr1.push(z+(z*3)/100)
+                 }
+                 updatedbankbalance[i].innerText=arr1[i];
+        }
+           
 }
